d4e6d5edf96aab2c6690be7e42b569cbb056580e
acupoframen1 <76802694+acupoframen@users.noreply.github.com>
2022-01-03 14:56:08 +0000

    my answer

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ n=int(input())
 data=[]
 for i in range(n):
   data.append(list(map(int,input().split())))
-dp=[[0,0,0] for _ in range(n)]
-dp[0]=data[0]
+dp=[data[0] for _ in range(n)]
+
 for i in range(1,n):
   dp[i][0]=data[i][0]+min(dp[i-1][1],dp[i-1][2])
-  print(data[i],dp[i-1],dp)
+  print(data[i],dp)
   dp[i][1]=data[i][1]+min(dp[i-1][0],dp[i-1][2])
   dp[i][2]=data[i][2]+min(dp[i-1][0],dp[i-1][1])
 
